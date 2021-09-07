1e0a82bdd986f0f74c27c88437c36c8bc30e04c8
Jan Václavík <jan.vaclavik@belcode.cz>
2021-09-08 00:33:42 +0200

    add update timestamp

--- src/screens/ConversionScreen.tsx
+++ src/screens/ConversionScreen.tsx
@@ -1,10 +1,16 @@
+import moment from 'moment';
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import useRates from '../hooks/useRates';
+
 export default function ConversionScreen() {
+  const rates = useRates();
+
   return (
     <View>
       <Text>Convertion Screen</Text>
+      <Text>{`Last update at ${moment(rates.dataUpdatedAt).format('HH:mm - DD. MM. YYYY')}`}</Text>
     </View>
   );
 }
--- src/screens/RatesScreen.tsx
+++ src/screens/RatesScreen.tsx
@@ -1,5 +1,6 @@
+import moment from 'moment';
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { Button, ScrollView, Text, View } from 'react-native';
 
 import useRates from '../hooks/useRates';
 
@@ -9,11 +10,17 @@ export default function RatesScreen() {
   return (
     <ScrollView>
       <Text>Rates Screen</Text>
-      <View>
-        {rates.data?.map((item) => (
-          <Text key={item.code}>{`${item.country}: 1 ${item.currency} = ${item.rate / item.quantity} CZK`}</Text>
-        ))}
-      </View>
+      <Text>{`Last update at ${moment(rates.dataUpdatedAt).format('HH:mm - DD. MM. YYYY')}`}</Text>
+      {rates.isLoading ? (
+        <Text>loading ...</Text>
+      ) : (
+        <View>
+          {rates.data?.map((item) => (
+            <Text key={item.code}>{`${item.country}: 1 ${item.currency} = ${item.rate / item.quantity} CZK`}</Text>
+          ))}
+        </View>
+      )}
+      <Button onPress={() => rates.refetch()} title="Refetch" />
     </ScrollView>
   );
 }

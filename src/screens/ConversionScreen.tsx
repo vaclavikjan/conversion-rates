cff28d492e063ec5464b390b29e4cdb3a1d66982
Jan Václavík <jan.vaclavik@belcode.cz>
2021-09-08 13:01:44 +0200

    add conversion form

--- src/screens/ConversionScreen.tsx
+++ src/screens/ConversionScreen.tsx
@@ -1,16 +1,38 @@
 import moment from 'moment';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
 
 import useRates from '../hooks/useRates';
+import { IRate } from '../types/entities';
 
 export default function ConversionScreen() {
   const rates = useRates();
+
+  const [currencyInput, setCurrencyInput] = useState(1);
+  const [selectedConversionCurrency, setSelectedConversionCurrency] = useState<IRate | null>(null);
+
+  useEffect(() => {
+    if (rates.data?.length) {
+      setSelectedConversionCurrency(rates.data[0]);
+    }
+  }, [rates.isLoading]);
 
   return (
     <View>
       <Text>Convertion Screen</Text>
       <Text>{`Last update at ${moment(rates.dataUpdatedAt).format('HH:mm - DD. MM. YYYY')}`}</Text>
+
+      <View>
+        <TextInput
+          keyboardType="number-pad"
+          onChangeText={(text) => setCurrencyInput(parseFloat(text) || 0)}
+          value={currencyInput.toString()}
+          returnKeyType="done"
+        />
+        {selectedConversionCurrency && (
+          <Text>{`${currencyInput / selectedConversionCurrency.rate} ${selectedConversionCurrency.currency}`}</Text>
+        )}
+      </View>
     </View>
   );
 }
